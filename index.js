c51002ca6b077109bba092fb9de274e653082345
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-08 23:10:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function shout(string){return string.toUpperCase(
 function whisper(string){return string.toLowerCase(
 )
 }
-function logShout(string){string.toUpperCase(
-)
+function logShout(string){ console.log(string.toUpperCase(
+))
 }
